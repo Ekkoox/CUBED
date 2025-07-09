1be9f76e7c09758a1cf5baeb1b922f587feffdb8
dsatge <deborah.satge@yahoo.fr>
2025-07-09 15:56:35 +0200

    floor and ceiling responsible to maps

--- srcs/display/floor_ceiling.c
+++ srcs/display/floor_ceiling.c
@@ -6,7 +6,7 @@
 /*   By: dsatge <dsatge@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 15:46:37 by dsatge            #+#    #+#             */
-/*   Updated: 2025/07/03 17:29:36 by dsatge           ###   ########.fr       */
+/*   Updated: 2025/07/09 15:55:23 by dsatge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +50,12 @@ static int	gener_sky(t_cubed cube, int x, int y)
 	if (ptr_sky == NULL)
 		return (ft_printf(2, "Error: mlx failure\n"), EXIT_FAILURE);
 	sky = mlx_get_data_addr(ptr_sky, &bpp, &width, &endian);
-	fill_image_color(sky, 4282009, WIDTH, HEIGHT / 2);
+	fill_image_color(sky, cube.pixel_data->ceiling_color, WIDTH, HEIGHT / 2);
 	mlx_put_image_to_window(cube.mlx, cube.win, ptr_sky, x, y);
 	mlx_destroy_image(cube.mlx, ptr_sky);
 	return (EXIT_SUCCESS);
 }
+
 
 static int	gener_floor(t_cubed cube, int x, int y)
 {
@@ -71,7 +72,7 @@ static int	gener_floor(t_cubed cube, int x, int y)
 	if (ptr_floor == NULL)
 		return (ft_printf(2, "Error: mlx failure\n"), EXIT_FAILURE);
 	floor = mlx_get_data_addr(ptr_floor, &bpp, &width, &endian);
-	fill_image_color(floor, 7030076, WIDTH, HEIGHT / 2);
+	fill_image_color(floor, cube.pixel_data->floor_color, WIDTH, HEIGHT / 2);
 	mlx_put_image_to_window(cube.mlx, cube.win, ptr_floor, x, y);
 	mlx_destroy_image(cube.mlx, ptr_floor);
 	return (EXIT_SUCCESS);
