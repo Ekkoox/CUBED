5955f49b963b02a1de1e9e171b433d5572724d6f
dsatge <deborah.satge@yahoo.fr>
2025-09-03 17:43:02 +0200

    fixed slinding

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: dsatge <dsatge@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:14:31 by enschnei          #+#    #+#             */
-/*   Updated: 2025/09/03 16:34:56 by dsatge           ###   ########.fr       */
+/*   Updated: 2025/09/03 17:36:25 by dsatge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,6 +144,9 @@ typedef struct s_player
 
 typedef struct s_ray
 {
+	double					ray_len_hit;
+	double					ray_lenx_hit;
+	double					ray_leny_hit;
 	double					x_hit;
 	double					y_hit;
 	int						dda;
